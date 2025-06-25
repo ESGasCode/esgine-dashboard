614e52a2c02ac17106e3642441ec1d581cad604f
Isaiah Owolabi <io@esgascode.com>
2025-06-25 20:05:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -38,10 +38,6 @@ from PyPDF2 import PdfReader
 st.write("✅ PdfReader module loaded:", PdfReader)
 st.write("✅ docx module loaded:", docx.__name__)
 
-# --- Add Backend to Path ---
-backend_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'esgine-backend'))
-sys.path.insert(0, backend_path)
-
 # --- Local Modules ---
 from parser.local_evaluator import load_yaml_rule, evaluate_rule
 
